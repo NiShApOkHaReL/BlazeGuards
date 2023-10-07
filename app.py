ad94ebcd609762b17b3d47aea33ac0d37a764b30
Nisha Pokharel <nishapokharel2018@gmail.com>
2023-10-07 11:38:59 +0545

    query

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 import streamlit as st
+
+
+
+
+
+
+
+
+st.title("BlazeGuards: Fire Management Solutions")
+st.map(use_container_width = True)
+
+active_query = "SELECT address, fire_intensity, population_density, sensitive_areas, status FROM blazeguards.submissions where status = 'Active';"
+operation_query = "SELECT address, fire_intensity, population_density, sensitive_areas, status FROM blazeguards.submissions where status = 'In-Operation';"
+control_query = "SELECT address, fire_intensity, population_density, sensitive_areas, status FROM blazeguards.submissions where status = 'Controlled';"
+
+
